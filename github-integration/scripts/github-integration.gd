5d3dfe6edb25c51a69a78430501cb6478f167beb
fenix-hub <n.santilio97@gmail.com>
2019-09-13 02:57:37 +0200

    Update 0.1.5

--- github-integration/scripts/github-integration.gd
+++ github-integration/scripts/github-integration.gd
@@ -1,12 +1,11 @@
-tool
-extends EditorPlugin
-
 # ----------------------------------------------
 #            ~{ GitHub Integration }~
 # [Author] Nicolò "fenix" Santilio 
 # [github] fenix-hub/godot.git-integration
 # [version] 0.0.1
 # [date] 2019 - 
+# 
+# {versioning: <grandi-cambiamenti>.<nuovi-contenuti>.<fix-patch> }
 
 
 
@@ -14,12 +13,16 @@ extends EditorPlugin
 
 # -----------------------------------------------
 
+tool
+extends EditorPlugin
 var doc
 
 func _enter_tree():
 	doc = preload("../scenes/GitHub.tscn").instance()
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR,doc)
+	add_autoload_singleton("UserData","res://addons/github-integration/scripts/user_data.gd")
 
 func _exit_tree():
 	remove_control_from_docks(doc)
+	remove_autoload_singleton("UserData")
 	doc.queue_free()
